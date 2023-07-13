74f7fb01fceca650b6bc6bca44e6f7f10ccfa35c
Yirsis Serrano <yirsishertz02@gmail.com>
2023-07-12 21:59:12 -0600

    ..

--- 04-todo-list/src/components/AvatarSidebar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-navigation-drawer theme="dark" rail permanent>
-    <v-list-item
-      nav
-      prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
-    ></v-list-item>
-  </v-navigation-drawer>
-</template>
--- 04-todo-list/src/views/ReportView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Report page</h1>
-</template>
